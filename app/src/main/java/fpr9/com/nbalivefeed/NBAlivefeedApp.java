5e16a7d2d940d7c15ff92fbc7a57ba67c952aba3
FranPR9 <lfranciscopr@gmail.com>
2016-11-08 14:15:16 -0600

    keys

--- app/src/main/java/fpr9/com/nbalivefeed/NBAlivefeedApp.java
+++ app/src/main/java/fpr9/com/nbalivefeed/NBAlivefeedApp.java
@@ -31,8 +31,8 @@ import io.fabric.sdk.android.Fabric;
 public class NBAlivefeedApp extends Application {
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "SrWIeCbiqBAHSdPxbfVVAod7z";
-    private static final String TWITTER_SECRET = "003SoYlZrDKwIwY7najuuIsoeJWPdOgvq6YzhIr0atvihMqyZM";
+    private static final String TWITTER_KEY = BuildConfig.TWITTER_KEY;
+    private static final String TWITTER_SECRET = BuildConfig.TWITTER_SECRET;
 
 
     @Override
